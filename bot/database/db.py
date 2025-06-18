99dd65ba7f3a4e7305877d590313b7463e7919ae
sargisss <mangasaryan1645@ilcoud.com>
2025-06-18 14:05:56 +0400

    server

--- bot/database/db.py
+++ bot/database/db.py
@@ -6,7 +6,20 @@ from bot.database.models import Base
 
 load_dotenv()
 
-engine = create_async_engine(url=os.getenv("DATABASE_URL"),echo=True)
+def get_database_url():
+    user = os.getenv("MYSQL_USER", "root")
+    password = os.getenv("MYSQL_PASSWORD", "admin111")
+    host = os.getenv("MYSQL_HOST", "localhost")
+    port = os.getenv("MYSQL_PORT", "3306")
+    db = os.getenv("MYSQL_DB", "somnialog")
+    return f"mysql+aiomysql://{user}:{password}@{host}:{port}/{db}"
+
+DATABASE_URL = get_database_url()
+
+engine = create_async_engine(
+    url=os.getenv("DATABASE_URL"),
+    connect_args={"connect_timeout": 10},
+    pool_pre_ping=True,)
 async_session = async_sessionmaker(engine)
 
 async def create_db():
